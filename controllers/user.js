6a0c1561626486f017551d3b36397cbb4077d999
Abhisheksah12 <aarav12sah@gmail.com>
2023-07-06 20:21:42 +0530

    imported errorhandler

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
-
+import ErrorHandler from "../middlewares/error.js";
 import { sendCookie } from "../utils/features.js";
 
 
